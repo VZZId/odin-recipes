30c338519879b3c36ba4eee64a77f65c580e1858
VZZId <166499783+VZZId@users.noreply.github.com>
2024-05-06 17:50:35 +0100

    feat: New name for website

--- scripts/title.js
+++ scripts/title.js
@@ -2,7 +2,7 @@
 let filename = window.location.pathname.split('/').pop().split('.')[0];
 
 // Your main website title
-let websiteTitle = " - Odin Recipes"; 
+let websiteTitle = " - Trewick's Recipes"; 
 
 // Combine and set the title
 document.title = filename[0].toUpperCase() + filename.slice(1) + websiteTitle;
