356c5edf0a43ff94d7b5f539da9f3bdc663daa70
VZZId <166499783+VZZId@users.noreply.github.com>
2024-05-06 19:21:46 +0100

    fix: Revert site to old name "Odin Recipes"

--- scripts/title.js
+++ scripts/title.js
@@ -2,7 +2,7 @@
 let filename = window.location.pathname.split('/').pop().split('.')[0];
 
 // Your main website title
-let websiteTitle = " - Trewick's Recipes"; 
+let websiteTitle = " - Odin Recipes"; 
 
 // Combine and set the title
 document.title = filename[0].toUpperCase() + filename.slice(1) + websiteTitle;
